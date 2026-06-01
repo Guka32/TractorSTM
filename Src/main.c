0b22ce5703cf900c22528923cf3d2b45f59a10d3
Guka32 <gutierrez.12193@gmail.com>
2026-05-31 19:00:42 -0600

    Reload

--- Src/main.c
+++
@@ -1,8 +0,0 @@
-/**
- ******************************************************************************
- * @file           : main.c
- * @brief          : This file is empty - main() is defined in main_uart.c
- ******************************************************************************
- */
-
-/* main() is defined in main_uart.c - this file intentionally left empty */
